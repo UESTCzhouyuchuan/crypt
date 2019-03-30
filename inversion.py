e99c044cbac60c985b1d56bb03e85aa3f652a4b1
xinxin <1738733078@qq.com>
2019-03-30 16:55:04 +0800

    repair

---
+++ inversion.py
@@ -0,0 +1,28 @@
+#inversion.py
+from companionMatrix import *
+from multiAddMatrix import * 
+#求出矩阵的逆
+def inversion(A):
+        companion = companionMatrix(A)
+        det = detMatrix(A)%26
+        t=OGLD_pro(det,26)
+        det=t['x']
+        for i in range(A.shape[0]):
+                for j in range(i+1):
+                        companion[i][j] = (companion[i][j]*det)%26
+                        if (i != j):
+                                companion[j][i] = (companion[j][i]*det)%26
+                                temp = companion[i][j]
+                                companion[i][j] = companion[j][i]
+                                companion[j][i] = temp
+        return companion
+"""扩展的欧几里得算法，用于求矩阵行列式的逆"""
+def OGLD_pro(a,b):
+    if (b==0):
+        return {'d':a,'x':1,'y':0}
+    else:
+        t=OGLD_pro(b,a%b)
+        temp=t['x']
+        t['x']=t['y']
+        t['y']=temp-(a//b)*t['y']
+        return t
